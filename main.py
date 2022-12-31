bdac9aafac87ec683106585fea0e2533b7732cd3
yzqzss <30341059+yzqzss@users.noreply.github.com>
2022-12-31 22:30:01 +0800

    今年就不鞭尸了
    总不能把人家几十年一直挂着把……

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ markdown = """# 2022年终总结
 
 > 2021 年的年终总结见：[saveweb/review-2021](https://github.com/saveweb/review-2021)
 
-本项目将长期维护（直到2024年初），因为[以往的经验](https://t.me/blogrsslist/269)告诉我——有些博主的年终总结可能要[拖拉个半年才写得完](https://idealclover.top/archives/627/)，还有些博主明明都年中了才写完了上一年的年终总结，又觉得不好意思[而自己把文章发布时间改成年初](https://github.com/saveweb/review-2021/commit/558ba32ef42025dd1bf2da31c2c1fb60e05e2524)，掩耳盗铃。有趣有趣……
-
-链接后面的数字是我们对每个博客分配的博客ID，有什么用呢？自行探索其它仓库。
+与去年一样，本项目将长期维护（直到2024年初）。
 
 **想要添加您的年终总结？请发 Issue 或编辑 metadata.md 发PR（不需要填写博客ID）。**
 
 ---
 
-### 相关链接:
+### 相关链接
 
 | [Save The Web's Telegram Channel](https://t.me/saveweb) | [独立博客&播客全订阅's Telegram Channel](https://t.me/blogrsslist) | [Github Org](https://github.com/saveweb)
 | --- | --- | --- |
 
-### 友情链接:
+### 广告
 
-- 待确定
+[我们给 1700 多个中文独立博客做了每日备份](https://blog.save-web.org/blog/2022/07/19/震惊，stwp-竟然给-1700-多个中文独立博客做了每日备份/)  
+
+> 实际频率是 每几天/每一两周 推送一次链接。
 
 ---
 
